86af36c23b57cee087c3be10ed417572f04e0ec6
Sergey Terekhin <sergeyter@gmail.com>
2016-11-14 10:26:26 +0300

    Update ConsoleHintedInput.cs

--- ConsoleHintedInput.cs
+++ ConsoleHintedInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace AS.Concept_vSdk.Cli
+namespace ConsoleHints
 {
     public class ConsoleHintedInput
     {
